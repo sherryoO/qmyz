194cfddd38fa3d71d0808313a2a427a58edfb6ec
sherroO <815793669@qq.com>
2017-03-29 09:24:03 +0800

    qmyz
    更新底部公共菜单路径地址

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,6 +1,6 @@
 // 底部菜单，包含二维码和个人中心
 document.writeln("<footer>");
-document.writeln("	<a href=\'../other/fxyz_zbz.html\' class=\'to_fxyz\'>放心易装，真保障＞</a>");
+document.writeln("	<a href=\'../common/fxyz_zbz.html\' class=\'to_fxyz\'>放心易装，真保障＞</a>");
 document.writeln("	<a href=\'javascript:void(0);\' class=\'qr_code\'>");
 document.writeln("		<img src=\'../../public/images/erweima.png\'>");
 document.writeln("	</a>");
